c6ee56952f7ef07f7310f58e426243caac6939ea
Osmir Pedro <osmir.pedro@invenis.com.br>
2025-07-28 20:53:06 -0300

    module form create

---
+++ resources/views/components/form.blade.php
@@ -0,0 +1,28 @@
+<div class="modal fade" id="{{ $formId }}" tabindex="-1" aria-labelledby="formModal" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <form method="POST" action='{{ route("$route") }}''>
+                @csrf
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="modalLabel">New {{ $title }}</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    @foreach ($inputs as $input)
+                        <div class="mb-3">
+                            <label for="{{ $input['name'] }}" class="form-label">{{ $input['label'] }}:</label>
+                            <input type="{{ $input['type'] }}" class="form-control" id="{{ $input['id'] }}" name="{{ $input['name'] }}" aria-describedby="help">
+                        </div>
+                    @endforeach
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Save</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+
+
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,7 +13,7 @@
           <div class="row align-items-center m-5 px-4">
               <div class="col p-4">
                 <div class="row">
-                  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Create Product</button>
+                  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#productFormModal">Create Product</button>
                 </div>
                 <br>
                 <div class="row">
@@ -29,7 +29,7 @@
               </div>
               <div class="col p-4">
                 <div class="row">
-                  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#storageModal">Create storage</button>
+                  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#storageFormModal">Create storage</button>
                 </div><br>
                 <div class="row">
                   @include('components.table',[
@@ -43,7 +43,7 @@
               </div>
               <div class="col p-4">
                 <div class="row">
-                  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#couponModal">Create coupons</button>
+                  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#couponFormModal">Create coupons</button>
                 </div><br>
                 <div class="row">
                   @include('components.table', [
@@ -59,8 +59,42 @@
             </div>
       </div>
       
-      @include('product.create') 
-      @include('storage.form') 
-      @include('coupon.create')
+      @include('components.form',[
+        'formId'=> 'productFormModal',
+        'title' => 'Product',
+        'route' => "createProduct",
+        'inputs' => [
+            1 => ['id' => 'name', 'name'  => 'name', 'type'  => 'text', 'label' => 'Product name'],
+            2 => ['id' => 'price', 'name'  => 'price', 'type'  => 'text', 'label' => 'Price'],
+            3 => ['id' => 'quantity', 'name'  => 'quantity', 'type'  => 'text', 'label' => 'Quantity'],
+          ]
+      ])
+
+      @include('components.form',[
+        'formId'=> 'storageFormModal',
+        'title' => 'Storage',
+        'route' => "createStorage",
+        'inputs' => [
+            '0' => [
+              'id'    => 'name',
+              'name'  => 'name',
+              'type'  => 'text',
+              'label' => 'Storage name'
+              ]
+          ]
+      ])
+
+      @include('components.form',[
+        'formId'=> 'couponFormModal',
+        'title' => 'Coupon',
+        'route' => "createCoupon",
+        'inputs' => [
+            1 => ['id' => 'name', 'name'  => 'name', 'type'  => 'text', 'label' => 'Coupon name'],
+            2 => ['id' => 'porcetage', 'name'  => 'porcetage', 'type'  => 'text', 'label' => 'Porcetage'],
+            3 => ['id' => 'quantity', 'name'  => 'quantity', 'type'  => 'text', 'label' => 'Quantity'],
+            4 => ['id' => 'valid', 'name'  => 'valid', 'type'  => 'text', 'label' => 'Valid date'],
+          ]
+      ])
+
   </body>
 </html>
